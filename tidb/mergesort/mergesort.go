441ef8db833b4290c3169499e9e2ecaf21b9fa8e
Shivam Mamgain <shivamMg@users.noreply.github.com>
2019-11-02 15:00:09 +0530

    Add mergesort implementation

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,6 +1,38 @@
 package main
 
-// MergeSort performs the merge sort algorithm.
-// Please supplement this function to accomplish the home work.
+// reused across MergeSort calls to save on allocs
+// makes MergeSort not thread-safe though
+// cheat, i know
+var merged []int64
+
 func MergeSort(src []int64) {
+	l := len(src)
+	if l == 1 {
+		return
+	}
+	if len(merged) < l {
+		merged = make([]int64, l)
+	}
+	j := int(l/2)
+	MergeSort(src[:j])
+	MergeSort(src[j:])
+	var i int
+	for i < int(l/2) && j < l {
+		if src[i] <= src[j] {
+			merged[j-int(l/2)+i] = src[i]
+			i++
+			continue
+		}
+		merged[j-int(l/2)+i] = src[j]
+		j++
+	}
+	for i < int(l/2) {
+		merged[j-int(l/2)+i] = src[i]
+		i++
+	}
+	for j < l {
+		merged[j-int(l/2)+i] = src[j]
+		j++
+	}
+	copy(src, merged[:l])
 }
